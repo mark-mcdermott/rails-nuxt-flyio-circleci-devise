aa77dd9827fca691ab971a97252183c1033f77fa
Mark McDermott <mark@markmcdermott.io>
2025-01-27 05:18:12 -0600

    Specify Vitest include/exclude paths

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,9 @@
 /// frontend/vitest.config.ts
 
 import { defineVitestConfig } from '@nuxt/test-utils/config'
-export default defineVitestConfig({})
+export default defineVitestConfig({
+  test: {
+    include: ['spec/components/**/*.spec.ts'], // Only component tests
+    exclude: ['spec/e2e/**/*'], // Exclude e2e tests
+  },
+})
